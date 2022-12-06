b1f578b8b68e9d4849863803cc666ac5a5f8fd95
Nicholas Blott <blottn@arista.com>
2022-12-06 11:54:28 +0000

    fix: Tidy up 06

--- 06/index.js
+++ 06/index.js
@@ -7,16 +7,10 @@ input.pop();
 const lastX = (from, x) => [...new Array(x).keys()]
     .reduce((acc, n) => ({[input[from - n]]: true, ...acc}), {}) 
 
-
-const last4 = (from) => lastX(from, 4);
-const last14 = (from) => last14(from, 14);
-
 let solve = (len) => input.map((_, i) => i > len - 2 ? i : false)
     .filter(b => b)
     .map(i => [Object.keys(lastX(i, len)).length == len, i + 1])
     .reduce(([yes, where], [yesNext, whereNext]) => yesNext && !yes ? [yesNext, whereNext] : [yes, where]);
-
-
 
 console.log(solve(4));
 console.log(solve(14));
